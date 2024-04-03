34ae8cd0aea0194febaa56b56afeeb701b2415c4
Animesh Gupta <animesh2411@gmail.com>
2024-04-04 01:51:38 +0530

    Create ContainsDuplicate.java

---
+++ src/Solutions/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package Solutions;
+
+import java.util.*;
+
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        // Set<Integer> numSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        // if(numSet.size() == nums.length) {
+        //     return false;
+        // }
+        // return true;
+      
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
