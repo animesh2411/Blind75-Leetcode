87bc3e4df1acf5100f5ba78235325becd52cd6d1
Animesh Gupta <animesh2411@gmail.com>
2024-04-12 00:21:33 +0530

    SumOfTwoIntegersWithoutArithmeticOperations

---
+++ src/stringProblems/SumOfTwoIntegersWithoutArithmeticOperations.java
@@ -0,0 +1,19 @@
+package stringProblems;
+
+public class SumOfTwoIntegersWithoutArithmeticOperations {
+    public int getSum(int a, int b) {
+
+        // Base case: if b is 0, return a
+        if (b == 0)
+            return a;
+
+        // Sum without considering carry
+        int sum = a ^ b;
+
+        // Carry is shifted by one so that adding it to sum gives the required sum
+        int carry = (a & b) << 1;
+
+        // Recursively call getSum with sum and carry until carry becomes 0
+        return getSum(sum, carry);
+    }
+}
